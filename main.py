d50f72deeaf6d539b2a2a6772154734c607155f3
kumail.r7 <kumail.r@shyenatechyarns.com>
2023-02-23 13:29:46 +0400

    Added new code

---
+++ main.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Kumail Rizvi "+ Fore.YELLOW+ Back.BLUE+"I am your Devops Instructor")
+print(Back.CYAN+"Hi My name is Kumail Rizvi")
+print(Fore.RED + Back.BLUE+ "Hi My name is Kumail Rizvi")
